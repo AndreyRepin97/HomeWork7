43c450eafa2d88c6b41d050958201e3561ae75ad
porwa <o-la-la-repka@yandex.ru>
2022-04-14 22:10:29 +0300

    CompleteHomeWork7

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,5 +3,38 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
+
+        //task1
+        System.out.println("Task1");
+
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = lastName+middleName+firstName;
+
+        System.out.println("ФИО сотрудника - "+fullName);
+
+        //task2
+        System.out.println("Task2");
+
+        String bigName = fullName.toUpperCase();
+
+        System.out.println(bigName);
+
+        //task3
+        System.out.println("Task3");
+
+        fullName = "Иванов Семён Сёменович";
+        System.out.println(fullName);
+
+        char[] bukvaArr = fullName.toCharArray();
+
+        for (int i=0;i<bukvaArr.length;i++){
+            if (bukvaArr[i]=='ё') {
+                bukvaArr[i] = 'е';
+            }
+        }
+        String fullNameE = new String(bukvaArr);
+        System.out.println(fullNameE);
     }
 }
